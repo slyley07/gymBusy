7e2db4c3cf565bbcc94850243e757ef7978e2827
Bradley <showalter.bradley@gmail.com>
2015-04-23 11:09:08 -0400

    Create Gemfile. Bundle gems. Add and require to main.rb. Create views, public, javascript, images, and css folders.

--- main.rb
+++ main.rb
@@ -1,1 +1,18 @@
-test
+require 'sinatra'
+require 'mandrill'
+
+get '/' do
+	erb :home
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/classes' do
+	erb :classes
+end
+
+get '/join_us' do
+ erb :join
+end
