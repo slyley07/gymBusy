60067eef14d7bb253688f245feec36c7a139e111
mamosken <marcelamosken@gmail.com>
2015-04-26 14:10:08 -0400

    added animation in classes images and captions, code inserted in scrip.js file, changes ids and class tags in classes page

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -18,12 +18,89 @@ $(document).ready(function() {
 		})
 	// $("")
 
-	 $("#img1").hover(function(){
-        $("#img2").css("opacity","0.2");
-        },function(){
-      $("#img2").css("opacity","1");
+	 $(".img1").hover(function(){
+	     $(".img2").css("opacity","0.2");
+	     $(".img3").css("opacity","0.2");
+	     $(".img4").css("opacity","0.2");
+	     $(".img5").css("opacity","0.2");
+	     $(".img6").css("opacity","0.2");
+	        },function(){
+	     	 $(".img2").css("opacity","1");
+	     	 $(".img3").css("opacity","1");
+	         $(".img4").css("opacity","1");
+	    	 $(".img5").css("opacity","1");
+	         $(".img6").css("opacity","1");
     });
+	 	 $(".img2").hover(function(){
+	     $(".img1").css("opacity","0.2");
+	     $(".img3").css("opacity","0.2");
+	     $(".img4").css("opacity","0.2");
+	     $(".img5").css("opacity","0.2");
+	     $(".img6").css("opacity","0.2");
+	        },function(){
+	     	 $(".img1").css("opacity","1");
+	     	 $(".img3").css("opacity","1");
+	         $(".img4").css("opacity","1");
+	    	 $(".img5").css("opacity","1");
+	         $(".img6").css("opacity","1");
 
+    });
+	 	 	 $(".img3").hover(function(){
+	     $(".img1").css("opacity","0.2");
+	     $(".img2").css("opacity","0.2");
+	     $(".img4").css("opacity","0.2");
+	     $(".img5").css("opacity","0.2");
+	     $(".img6").css("opacity","0.2");
+	        },function(){
+	     	 $(".img1").css("opacity","1");
+	     	 $(".img2").css("opacity","1");
+	         $(".img4").css("opacity","1");
+	    	 $(".img5").css("opacity","1");
+	         $(".img6").css("opacity","1");
+
+    });
+	 $(".img4").hover(function(){
+	     $(".img1").css("opacity","0.2");
+	     $(".img2").css("opacity","0.2");
+	     $(".img3").css("opacity","0.2");
+	     $(".img5").css("opacity","0.2");
+	     $(".img6").css("opacity","0.2");
+	        },function(){
+	     	 $(".img2").css("opacity","1");
+	     	 $(".img3").css("opacity","1");
+	         $(".img1").css("opacity","1");
+	    	 $(".img5").css("opacity","1");
+	         $(".img6").css("opacity","1");
+
+    });
+	 	 $(".img5").hover(function(){
+	     $(".img2").css("opacity","0.2");
+	     $(".img3").css("opacity","0.2");
+	     $(".img4").css("opacity","0.2");
+	     $(".img1").css("opacity","0.2");
+	     $(".img6").css("opacity","0.2");
+	        },function(){
+	     	 $(".img2").css("opacity","1");
+	     	 $(".img3").css("opacity","1");
+	         $(".img4").css("opacity","1");
+	    	 $(".img1").css("opacity","1");
+	         $(".img6").css("opacity","1");
+
+    });
+	 	 $(".img6").hover(function(){
+	     $(".img2").css("opacity","0.2");
+	     $(".img3").css("opacity","0.2");
+	     $(".img4").css("opacity","0.2");
+	     $(".img5").css("opacity","0.2");
+	     $(".img1").css("opacity","0.2");
+	        },function(){
+	     	 $(".img2").css("opacity","1");
+	     	 $(".img3").css("opacity","1");
+	         $(".img4").css("opacity","1");
+	    	 $(".img5").css("opacity","1");
+	         $(".img1").css("opacity","1");
+
+    });
  //     $("a1").click(function(){
 	// $("#inner3").animate( { 
  //   	   opacity: 0.5,  high:"100%", width: "100%" 
